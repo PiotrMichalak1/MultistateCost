d3f9a1743146820f0ea98b4de5ebf9767d3b5112
PiotrMichalak1 <128514914+PiotrMichalak1@users.noreply.github.com>
2023-04-04 20:11:19 +0200

    Added initialization of repair cost and duration to parameters.

--- src/panels/MainPanel.java
+++ src/panels/MainPanel.java
@@ -8,13 +8,13 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainPanel extends JPanel {
-    public MainPanel(){
-        this.setPreferredSize(new Dimension(1200,675));
-        Parameters.getInstance();
+    public MainPanel() {
+        this.setPreferredSize(new Dimension(1200, 675));
+        Parameters parameters = Parameters.getInstance();
         addWeightedPanes();
     }
 
-    private void addWeightedPanes(){
+    private void addWeightedPanes() {
         this.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
         SettingsPanel settingsPanel = new SettingsPanel();
@@ -23,7 +23,7 @@ public class MainPanel extends JPanel {
         c.weighty = 1.0;
         c.gridx = 0;
         c.gridy = 0;
-        this.add(settingsPanel,c);
+        this.add(settingsPanel, c);
 
         SimulationPanel simulationPanel = new SimulationPanel();
         c.weightx = 6.0;
--- src/settings/Parameters.java
+++ src/settings/Parameters.java
@@ -4,18 +4,21 @@ public class Parameters {
 
     private static Parameters instance =null;
     private final int NUM_OF_STATES = 4;
-    private int REPAIR_COST_STEP = 10;
+    private final int  REPAIR_COST_STEP = 10;
+    private final int REPAIR_DURATION_STEP = 1;
     private int[][] repairCostMatrix;
+    private int[][] repairDurationMatrix;
 
     private Parameters() {
         initializeRepairCosts();
+        initializeRepairDuration();
     }
 
     public static Parameters getInstance() {
         if (instance ==null){
             instance = new Parameters();
         }
-        return instance;
+        return  instance;
     }
 
     private void initializeRepairCosts() {
@@ -25,9 +28,27 @@ public class Parameters {
             for (int j = i; j < repairCostMatrix[i].length; j++) {
                 repairCostMatrix[i][j] = repairCost;
                 repairCost += REPAIR_COST_STEP;
-                System.out.println(repairCostMatrix[i][j]+ "Is a cost");
             }
         }
+    }
+
+    private void initializeRepairDuration(){
+        repairDurationMatrix = new int[NUM_OF_STATES - 1][NUM_OF_STATES - 1];
+        for (int i = 0; i < repairDurationMatrix.length; i++) {
+            int repairDuration = REPAIR_DURATION_STEP;
+            for (int j = i; j < repairDurationMatrix[i].length; j++) {
+                repairDurationMatrix[i][j] = repairDuration;
+                repairDuration += REPAIR_DURATION_STEP;
+            }
+        }
+    }
+
+    public int getRepairCost(int fromState, int toState) {
+        return repairCostMatrix[toState-1][fromState-2];
+    }
+
+    public int getRepairDuration(int fromState, int toState) {
+        return repairDurationMatrix[toState-1][fromState-2];
     }
 
 }
